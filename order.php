1a1c381854ac3ef660be6cb021f33f8ecd1e0363
keuss le mojo <julio.nathan47@gmail.com>
2024-11-07 14:29:16 +0100

    ajout adresse et envoyé

--- order.php
+++ order.php
@@ -2,10 +2,12 @@
 
 require_once "./partials/_header.html";
 
-class order {
+class order
+{
 
     public $id;
     public $customerName;
+    public $deliveryAdress;
     public $status = "cart"; //je dis que status = cart de base
     public $totalPrice = 0; //que le prix total de base est 0
     public $products = []; //et que les produits seront dans un tableau
@@ -14,8 +16,10 @@ class order {
 //le construteur est une méthode "magique" car elle est appelée automatiquement
 //le constructeur est appelée quand un objet est crée pour cette classe
 //un objet crée pour une classe appelée "instance de class"
-    public function __construct($customerName) {
+    public function __construct($customerName, $deliveryAdress)
+    {
         $this->customerName = $customerName;
+        $this->deliveryAdress = $deliveryAdress;
         $this->id = uniqid();
     }
 
@@ -30,9 +34,11 @@ class order {
         }
     }
 
-    public function removeProduct() { //function qui retire le dernier produit ajouter
-        if ($this->status === "cart") {
-            array_pop($this->products);//je retire le dernier produit du tableau
+
+    public function removeProduct()
+    { //function qui retire le dernier produit ajouter
+        if ($this->status === "cart" && !empty($this->products)) {
+            array_pop($this->products);//je retire le dernier produit ajouter au tableau
             $this->totalPrice -= 2;//je retire 2 au prix total
         }
     }
@@ -43,25 +49,27 @@ class order {
             $this->status = "paid";
         }
     }
+
+    public function sendOrder()
+    {
+        if ($this->status === "paid") {
+            $this->status = "sent";
+            echo "Votre commande a bien été envoyée";
+        }
+    }
+
 }
+
+
 
 //je créé une instance de la classe order
 //c'est à dire un objet issu du plan de construction de la classe order
-$order1 = new order("Nathan"); //nouvelle commande
+$order1 = new order("Nathan", "bordeaux rue 15"); //nouvelle commande
 $order1->addProduct(); //j'ajoute un produit en plusieurs fois
 $order1->addProduct();
 $order1->removeProduct();//je retire le dernier produit ajouter
 $order1->addProduct();
 $order1->addProduct();
 $order1->pay();//et je paie
+$order1->sendOrder();
 var_dump($order1);
-
-$order2 = new order("Joe");
-$order2->addProduct();
-$order2->addProduct();
-$order2->addProduct();
-$order2->removeProduct();
-$order2->addProduct();
-$order2->addProduct();
-$order2->pay();
-var_dump($order2);
