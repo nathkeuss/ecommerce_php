b018c1dee296dce4bbe13b553a7762881e2817d9
keuss le mojo <julio.nathan47@gmail.com>
2024-11-07 12:47:03 +0100

    ajout d'un prénom à une commande

--- order.php
+++ order.php
@@ -1,7 +1,8 @@
 <?php
 
-class order
-{
+require_once "./partials/_header.html";
+
+class order {
 
     public $id;
     public $customerName;
@@ -10,10 +11,19 @@ class order
     public $products = []; //et que les produits seront dans un tableau
 
 
+//le construteur est une méthode "magique" car elle est appelée automatiquement
+//le constructeur est appelée quand un objet est crée pour cette classe
+//un objet crée pour une classe appelée "instance de class"
+    public function __construct($customerName) {
+        $this->customerName = $customerName;
+        $this->id = uniqid();
+    }
+
+
     public function addProduct()//fonction qui ajoute un produit et un prix si le status est cart.
     {   //le $this fait référence à l'object actuel
         //c'est à dire à $order1, ou $order2 etc
-        //donc à l'object actuel issu de la classe
+        //donc à l'objet actuel issu de la classe
         if ($this->status === "cart") {
             $this->products[] = "babybel";//j'ajoute un produit au tableau
             $this->totalPrice += 2;//j'ajoute 2 au totalPrice de base (donc 0)
@@ -35,7 +45,9 @@ class order
     }
 }
 
-$order1 = new order(); //nouvelle commande
+//je créé une instance de la classe order
+//c'est à dire un objet issu du plan de construction de la classe order
+$order1 = new order("Nathan"); //nouvelle commande
 $order1->addProduct(); //j'ajoute un produit en plusieurs fois
 $order1->addProduct();
 $order1->removeProduct();//je retire le dernier produit ajouter
@@ -43,3 +55,13 @@ $order1->addProduct();
 $order1->addProduct();
 $order1->pay();//et je paie
 var_dump($order1);
+
+$order2 = new order("Joe");
+$order2->addProduct();
+$order2->addProduct();
+$order2->addProduct();
+$order2->removeProduct();
+$order2->addProduct();
+$order2->addProduct();
+$order2->pay();
+var_dump($order2);
