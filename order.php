51936299cbbf5da4f949690828cd24160af67272
keuss le mojo <julio.nathan47@gmail.com>
2024-11-07 12:04:38 +0100

    ajout fonction remove

--- order.php
+++ order.php
@@ -11,10 +11,19 @@ class order
 
 
     public function addProduct()//fonction qui ajoute un produit et un prix si le status est cart.
-    {
+    {   //le $this fait référence à l'object actuel
+        //c'est à dire à $order1, ou $order2 etc
+        //donc à l'object actuel issu de la classe
         if ($this->status === "cart") {
-            $this->products[] = "babybel";
-            $this->totalPrice += 2;
+            $this->products[] = "babybel";//j'ajoute un produit au tableau
+            $this->totalPrice += 2;//j'ajoute 2 au totalPrice de base (donc 0)
+        }
+    }
+
+    public function removeProduct() { //function qui retire le dernier produit ajouter
+        if ($this->status === "cart") {
+            array_pop($this->products);//je retire le dernier produit du tableau
+            $this->totalPrice -= 2;//je retire 2 au prix total
         }
     }
 
@@ -26,18 +35,11 @@ class order
     }
 }
 
-
 $order1 = new order(); //nouvelle commande
 $order1->addProduct(); //j'ajoute un produit en plusieurs fois
 $order1->addProduct();
+$order1->removeProduct();//je retire le dernier produit ajouter
+$order1->addProduct();
+$order1->addProduct();
 $order1->pay();//et je paie
 var_dump($order1);
-
-$order2 = new order();
-$order2->addProduct();
-$order2->addProduct();
-$order2->addProduct();
-$order2->addProduct();
-$order2->addProduct();
-$order2->pay();
-var_dump($order2);
