bf7526a3558613c25430a2878bfeee607802438d
keuss le mojo <julio.nathan47@gmail.com>
2024-11-08 10:57:47 +0100

    vendor machine fini

--- vendormachine.php
+++ vendormachine.php
@@ -1,5 +1,8 @@
 <?php
-require_once ('./partials/_header.html');
+
+require_once('./config/config.php');
+require_once('./partials/_header.html');
+
 class VendorMachine
 {
 
@@ -39,14 +42,14 @@ class VendorMachine
     }
 
 
-    public function turnOn()
+    public function turnOn() //si turnOn est false il passe à true
     {
         if ($this->isOn == false) {
             $this->isOn = true;
         }
     }
 
-    function turnOff()
+    function turnOff()//après 18h la machine s'éteint
     {
         $now = new DateTime();
         $limitTime = new DateTime('18:00');
@@ -60,41 +63,50 @@ class VendorMachine
 
     public function buySnack($snackName)
     {
-        if ($this->isOn) {
-            foreach ($this->snacks as $index => $snack) {
-                if ($snack["name"] === $snackName && $snack["quantity"] >= 1) {
-                    $this->snacks[$index]["quantity"] = $this->snacks[$index]["quantity"] - 1;
-                    $this->cashAmount = $this->cashAmount += $snack["price"];
+        if ($this->isOn) { //la machine doit être allumé
+            foreach ($this->snacks as $index => $snack) { //boucle sur la liste des snacks
+                if ($snack["name"] === $snackName && $snack["quantity"] >= 1) { // vérifie si le nom du snack correspond et si la quantité est supérieur à 0
+                    $this->snacks[$index]["quantity"] = $this->snacks[$index]["quantity"] - 1; // décrément la quantité du snack
+                    $this->cashAmount = $this->cashAmount += $snack["price"]; //ajoute le prix du snack au montant de cashAmount
                 }
             }
-        } else {
-            throw new Exception("La machine n'est pas allumée.");
         }
     }
 
+
     public function shootWithFoot()
     {
-        if ($this->isOn === true) {
-            array_rand($this->snacks["name"]);
-            rand(1, $this->cashAmount);
-            $this->cashAmount -= rand(1, $this->cashAmount);
+        if ($this->isOn === true) { //si la machine est allumée
+            $randomCash = rand(1, $this->cashAmount); // prend un nombre aléatoire du total de cashAmount
+            $this->cashAmount -= $randomCash; // le décrément du total cashAmount
+            echo '<pre style="font-size: 18px; color:green;">';
+            echo "{$randomCash}€ sont tombés";
+            echo '</pre>';
+
+            $snackIndex = array_rand($this->snacks);//sélectionne un index aléatoire dans le tableau snacks
+            $snack = $this->snacks[$snackIndex];//récupère le snack aléatoire
+
+            if ($snack['quantity'] >= 1) { // si la quantité est supérieur ou égal à 1
+                $this->snacks[$snackIndex]["quantity"] - 1; //ça retire une quantité du snack en question
+
+                echo '<pre style="font-size: 18px; color:yellow;">';
+                echo "{$snack['name']} est tombé";
+                echo '</pre>';
+            }
         }
     }
 
 
 }
 
-//$order1 = new VendorMachine();
-//$order1->shootWithFoot();
-//var_dump($order1);
-
-$order2 = new VendorMachine();
-$order2->turnOn();
-$order2->buySnack("Twix");
+$order1 = new VendorMachine();
+$order1->buySnack("Twix");
+$order1->buySnack("Twix");
+$order1->buySnack("Twix");
+$order1->buySnack("Twix");
+$order1->buySnack("Bounty");
+$order1->buySnack("Snickers");
+$order1->shootWithFoot();
 echo '<pre style="font-size: 18px; color:white;">';
-var_dump($order2);
+var_dump($order1);
 echo '</pre>';
-
-//$order3 = new VendorMachine();
-//$order3->turnOn();
-//var_dump($order3);
